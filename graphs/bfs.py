795eb8420dc6072262b64f9f0d2ee995e663301c
Andrew A Lee <andrew@andrewleedev.com>
2026-02-10 00:36:10 -0800

    Adds graph BFS & DFS

---
+++ graphs/bfs.py
@@ -0,0 +1,50 @@
+def depth_first_search(graph, root, target):
+  visited = [False for i in range(len(graph))]
+  queue = [(root, [root])]
+
+  while len(queue):
+    vertex, path = queue.pop()
+    visited[vertex] = True
+
+    for edge in graph[vertex]:
+      index = edge["to"]
+
+      if index == target:
+        return path + [index]
+
+      if not visited[index]:
+        queue.append((index, path + [index]))
+
+  return None
+
+graph = [
+  [
+    { "to": 1, "weight": 3 },
+    { "to": 2, "weight": 1 },
+  ],
+  [
+    { "to": 4, "weight": 1 },
+  ],
+  [
+    { "to": 3, "weight": 7 },
+  ],
+  [],
+  [
+    { "to": 1, "weight": 1 },
+    { "to": 3, "weight": 5 },
+    { "to": 5, "weight": 2 },
+  ],
+  [
+    { "to": 2, "weight": 18 },
+    { "to": 6, "weight": 1 },
+  ],
+  [
+    { "to": 3, "weight": 1 },
+  ],
+]
+
+path = depth_first_search(graph, 0, 6)
+print(path)
+
+path = depth_first_search(graph, 0, 7)
+print(path)
---
+++ graphs/dfs.py
@@ -0,0 +1,43 @@
+def dfs_recursive(graph, vertex, target, visited, path):
+  path.append(vertex)
+
+  if target == vertex:
+    return True
+
+  visited[vertex] = True
+  edges = graph[vertex]
+
+  for i in range(len(edges)):
+    if not visited[i] and edges[i] > 0:
+      is_found = dfs_recursive(graph, i, target, visited, path)
+      if is_found:
+        return True
+
+  path.pop()
+  return False
+
+def depth_first_search(graph, root, target):
+  visited = [False for i in range(len(graph))]
+  path = []
+  is_found = dfs_recursive(graph, root, target, visited, path)
+
+  if is_found:
+    return path
+
+  return None
+
+graph = [
+  [0, 3, 1,  0, 0, 0, 0],
+  [0, 0, 0,  0, 1, 0, 0],
+  [0, 0, 7,  0, 0, 0, 0],
+  [0, 0, 0,  0, 0, 0, 0],
+  [0, 1, 0,  5, 0, 2, 0],
+  [0, 0, 18, 0, 0, 0, 1],
+  [0, 0, 0,  1, 0, 0, 1],
+]
+
+result = depth_first_search(graph, 0, 6)
+print(result)
+
+result = depth_first_search(graph, 0, 7)
+print(result)
