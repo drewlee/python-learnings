576580909723b6b7fd4ea32ff1c736e3cc090103
Andrew A Lee <andrew@andrewleedev.com>
2026-02-19 10:59:21 -0800

    Progress on data source

--- oscar-films/data.py
+++ oscar-films/data.py
@@ -25,6 +25,7 @@ categories = [
     "Best costume design",
 ]
 
+
 films = {
     "Sentimental Value": {
         "category": [
@@ -272,12 +273,502 @@ films = {
             "period",
         ],
     },
+    "Butcher's Stain": {
+        "category": ["Best live action short film"],
+        "cast_and_crew": [
+            "Meyer Levinson-Blount",
+            "Omar Sameer",
+            "Rona Toledano",
+        ],
+        "genre": [
+            "drama",
+            "international",
+            "short"
+        ],
+    },
+    "Jane Austen's Period Drama": {
+        "category": ["Best live action short film"],
+        "cast_and_crew": [
+            "Julia Aks",
+            "Steve Pinder",
+            "Ta'imua",
+            "Samantha Smart",
+            "Nicole Alyse Nelson",
+        ],
+        "genre": [
+            "comedy",
+            "period",
+            "short"
+        ],
+    },
+    "A Friend of Dorothy": {
+        "category": ["Best live action short film"],
+        "cast_and_crew": [
+            "Lee Knight",
+            "Alistair Nwachukwu",
+            "Oscar Lloyd",
+            "Stephen Fry",
+        ],
+        "genre": [
+            "comedy",
+            "drama",
+            "international",
+            "short",
+        ],
+    },
+    "The Singers": {
+        "category": ["Best live action short film"],
+        "cast_and_crew": [
+            "Sam A. Davis",
+            "Ivan Turgenev",
+            "Michael Young",
+            "Chris Smither",
+            "Will Harrington",
+        ],
+        "genre": [
+            "comedy",
+            "drama",
+            "music",
+            "short",
+        ],
+    },
+    "Two People Exchanging Saliva": {
+        "category": ["Best live action short film"],
+        "cast_and_crew": [
+            "Natalie Musteata",
+            "Alexandre Singh",
+            "Zar Amir Ebrahimi",
+            "Luàna Bajrami",
+            "Vicky Krieps",
+        ],
+        "genre": [
+            "drama",
+            "fantasy",
+            "international",
+            "short",
+            "sci-fi",
+        ],
+    },
+    "Blue Moon": {
+        "category": [
+            "Best actor",
+            "Best original screenplay",
+        ],
+        "cast_and_crew": [
+            "Richard Linklater",
+            "Robert Kaplow",
+            "Lorenz Hart",
+            "Elizabeth Weiland"
+            "Ethan Hawke",
+            "Bobby Cannavale",
+            "Andrew Scott",
+        ],
+        "genre": [
+            "biography",
+            "drama",
+            "music",
+            "period",
+        ],
+    },
+    "It Was Just an Accident": {
+        "category": [
+            "Best original screenplay",
+            "Best international feature film",
+        ],
+        "cast_and_crew": [
+            "Jafar Panahi",
+            "Nader Saeivar",
+            "Shadmehr Rastin",
+            "Vahid Mobasseri",
+            "Mariam Afshari",
+            "Ebrahim Azizi",
+        ],
+        "genre": [
+            "drama",
+            "international",
+            "mystery",
+            "thriller",
+        ],
+    },
+    "Marty Supreme": {
+        "category": [
+            "Best original screenplay",
+            "Best casting",
+            "Best production design",
+            "Best film editing",
+            "Best cinematography",
+            "Best actor",
+            "Best director",
+            "Best picture",
+            "Best costume design",
+        ],
+        "cast_and_crew": [
+            "Josh Safdie",
+            "Ronald Bronstein",
+            "Jennifer Venditti",
+            "Miyako Bellizzi",
+            "Timothee Chalamet",
+            "Gwyneth Paltrow",
+            "Odessa A'zion",
+        ],
+        "genre": [
+            "drama",
+            "sport",
+        ],
+    },
+    "Papillon (Butterfly)": {
+        "category": ["Best animated short film"],
+        "cast_and_crew": [
+            "Florence Miailhe",
+            "Marie Desplechin",
+            "Fayçal Safi",
+            "Jessica Jaouiche",
+        ],
+        "genre": [
+            "animation",
+            "biography",
+            "drama",
+            "international",
+            "short",
+            "sport",
+        ],
+    },
+    "Forevergreen": {
+        "category": ["Best animated short film"],
+        "cast_and_crew": [
+            "Nathan Engelhardt",
+            "Jeremy Spears",
+        ],
+        "genre": [
+            "animation",
+            "short",
+        ],
+    },
+    "The Girl Who Cried Pearls": {
+        "category": ["Best animated short film"],
+        "cast_and_crew": [
+            "Chris Lavis",
+            "Maciek Szczerbowski",
+            "Colm Feore",
+            "Gabrielle Dallaire",
+        ],
+        "genre": [
+            "animation",
+            "drama",
+            "fantasy",
+            "short",
+        ],
+    },
+    "Retirement Plan": {
+        "category": ["Best animated short film"],
+        "cast_and_crew": [
+            "John Kelly",
+            "Tara Lawall",
+            "Domhnall Gleeson",
+        ],
+        "genre": [
+            "animation",
+            "comedy",
+            "short",
+        ],
+    },
+    "The Three Sisters": {
+        "category": ["Best animated short film"],
+        "cast_and_crew": [
+            "Konstantin Bronzit",
+        ],
+        "genre": [
+            "animation",
+            "comedy",
+            "international",
+            "short",
+        ],
+    },
+    "The Secret Agent": {
+        "category": [
+            "Best casting",
+            "Best international feature film",
+            "Best actor",
+            "Best picture",
+        ],
+        "cast_and_crew": [
+            "Kleber Mendonça Filho",
+            "Gabriel Domingues",
+            "Wagner Moura",
+            "Robson Andrade",
+            "Rubens Santos",
+            "Licínio Januário",
+        ],
+        "genre": [
+            "drama",
+            "international",
+            "mystery",
+            "thriller",
+        ],
+    },
+    "Diane Warren: Relentless": {
+        "category": ["Best original song"],
+        "cast_and_crew": [
+            "Bess Kargman",
+            "Diane Warren",
+        ],
+        "genre": [
+            "documentary",
+            "music",
+        ],
+    },
+    "KPop Demon Hunters": {
+        "category": [
+            "Best original song",
+            "Best animated feature film",
+        ],
+        "cast_and_crew": [
+            "Chris Appelhans",
+            "Maggie Kang",
+            "Danya Jimenez",
+            "Hannah McMechan",
+            "Maggie Kang",
+            "Arden Cho",
+            "Ahn Hyo-seop",
+            "May Hong",
+        ],
+        "genre": [
+            "animation",
+            "action",
+            "adventure",
+            "comedy",
+            "fantasy",
+            "music",
+        ],
+    },
+    "Viva Verdi!": {
+        "category": ["Best original song"],
+        "cast_and_crew": [
+            "Yvonne Russo",
+            "Christine La Monte",
+            "Claudio Giombi",
+            "Tina Aliprandi",
+            "Chitose Matsumoto",
+        ],
+        "genre": [
+            "documentary",
+            "international",
+        ],
+    },
+    "The Alabama Solution": {
+        "category": ["Best documentary feature film"],
+        "cast_and_crew": [
+            "Andrew Jarecki",
+            "Charlotte Kaufman",
+        ],
+        "genre": ["documentary"],
+    },
+    "Come See Me in the Good Light": {
+        "category": ["Best documentary feature film"],
+        "cast_and_crew": [
+            "Ryan White",
+            "Pattie Gonia",
+            "Andrea Gibson",
+        ],
+        "genre": ["documentary"],
+    },
+    "Cutting Through Rocks": {
+        "category": ["Best documentary feature film"],
+        "cast_and_crew": [
+            "Sara Khaki",
+            "Mohammadreza Eyni",
+        ],
+        "genre": ["documentary", "international"],
+    },
+    "Mr. Nobody Against Putin": {
+        "category": ["Best documentary feature film"],
+        "cast_and_crew": [
+            "David Borenstein",
+            "Pavel Talankin",
+        ],
+        "genre": ["documentary", "international"],
+    },
+    "The Perfect Neighbor": {
+        "category": ["Best documentary feature film"],
+        "cast_and_crew": [
+            "Geeta Gandbhir",
+            "Alisa Payne",
+        ],
+        "genre": ["documentary"],
+    },
+    "All the Empty Rooms": {
+        "category": ["Best documentary short film"],
+        "cast_and_crew": [
+            "Fara Dabhoiwala",
+            "Bree Cook",
+        ],
+        "genre": ["documentary", "short"],
+    },
+    "Armed Only with a Camera: The Life and Death of Brent Renaud": {
+        "category": ["Best documentary short film"],
+        "cast_and_crew": [
+            "Craig Renaud",
+            "Brent Renaud",
+        ],
+        "genre": ["documentary", "short"],
+    },
+    "Children No More: 'Were And Are Gone'": {
+        "category": ["Best documentary short film"],
+        "cast_and_crew": [
+            "Tetyana Mishchenko",
+            "Sarah McCarthy",
+        ],
+        "genre": ["documentary", "short", "international"],
+    },
+    "The Devil is Busy": {
+        "category": ["Best documentary short film"],
+        "cast_and_crew": [
+            "Pamela Yates",
+            "Paco de Onis",
+        ],
+        "genre": ["documentary", "short", "international"],
+    },
+    "Perfectly a Strangeness": {
+        "category": ["Best documentary short film"],
+        "cast_and_crew": [
+            "Alison McAlpine",
+            "Sofian El Fani",
+        ],
+        "genre": ["documentary", "short"],
+    },
+    "Sirât": {
+        "category": [
+            "Best international feature film",
+            "Best sound",
+        ],
+        "cast_and_crew": [
+            "Oliver Laxe",
+            "Sergi Lopez",
+            "Bruno Nunez",
+        ],
+        "genre": ["drama", "international", "thriller"],
+    },
+    "The Voice of Hinad Rajab": {
+        "category": ["Best international feature film"],
+        "cast_and_crew": [
+            "Kaouther Ben Hania",
+            "Nadine Labaki",
+        ],
+        "genre": ["drama", "international"],
+    },
+    "Arco": {
+        "category": ["Best animated feature film"],
+        "cast_and_crew": [
+            "Ugo Bienvenu",
+            "Natalie Portman",
+        ],
+        "genre": ["animation", "adventure", "sci-fi"],
+    },
+    "Elio": {
+        "category": ["Best animated feature film"],
+        "cast_and_crew": [
+            "Domee Shi",
+            "Madeline Sharafian",
+            "Yonas Kibreab",
+        ],
+        "genre": ["animation", "adventure", "comedy"],
+    },
+    "Little Amélie or the Character of Rain": {
+        "category": ["Best animated feature film"],
+        "cast_and_crew": [
+            "Mailys Vallade",
+            "Liane-Cho Han",
+            "Lois Charpentier",
+        ],
+        "genre": ["animation", "drama", "international"],
+    },
+    "Zootopia 2": {
+        "category": ["Best animated feature film"],
+        "cast_and_crew": [
+            "Jared Bush",
+            "Byron Howard",
+            "Ginnifer Goodwin",
+            "Jason Bateman",
+        ],
+        "genre": ["animation", "adventure", "comedy"],
+    },
+    "F1": {
+        "category": [
+            "Best film editing",
+            "Best sound",
+            "Best visual effects",
+            "Best picture",
+        ],
+        "cast_and_crew": [
+            "Joseph Kosinski",
+            "Brad Pitt",
+            "Damson Idris",
+            "Claudio Miranda",
+        ],
+        "genre": ["action", "drama", "sport"],
+    },
+    "Avatar: Fire and Ash": {
+        "category": [
+            "Best visual effects",
+            "Best costume design",
+        ],
+        "cast_and_crew": [
+            "James Cameron",
+            "Sam Worthington",
+            "Zoe Saldana",
+        ],
+        "genre": ["action", "adventure", "sci-fi"],
+    },
+    "Jurassic World Rebirth": {
+        "category": ["Best visual effects"],
+        "cast_and_crew": [
+            "Gareth Edwards",
+            "Scarlett Johansson",
+            "Jonathan Bailey",
+        ],
+        "genre": ["action", "adventure", "sci-fi"],
+    },
+    "The Lost Bus": {
+        "category": ["Best visual effects"],
+        "cast_and_crew": [
+            "Paul Greengrass",
+            "Matthew McConaughey",
+            "America Ferrera",
+        ],
+        "genre": ["drama", "thriller"],
+    },
+    "If I Had Legs I'd Kick You": {
+        "category": [
+            "Best actress",
+        ],
+        "cast_and_crew": [
+            "Mary Bronstein",
+            "Rose Byrne",
+            "A$AP Rocky",
+            "Conan O'Brien",
+        ],
+        "genre": ["comedy", "drama"],
+    },
+    "Song Sung Blue": {
+        "category": ["Best picture"],
+        "cast_and_crew": [
+            "Craig Brewer",
+            "Hugh Jackman",
+            "Kate Hudson",
+            "Michael Imperioli",
+        ],
+        "genre": ["drama", "music"],
+    },
 }
+
 
 genres = [
     "action",
+    "adventure",
+    "animation",
     "biography",
     "comedy",
+    "documentary",
     "drama",
     "fantasy",
     "horror",
@@ -287,7 +778,7 @@ genres = [
     "period",
     "romance",
     "sci-fi",
-    "short film",
+    "short",
     "sport",
     "thriller",
 ]
